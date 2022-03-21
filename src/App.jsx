df32d171834cf183d9fe4ae082695e0cad7f5cef
d22lbn <d22lbn@gmail.com>
2022-03-21 11:00:09 +0300

    Made the phone and computer on the home screen

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,36 @@ function App() {
                     </nav>    
                 </div>
             </header>
-            <main></main>
+
+
+
+            <main>
+                <div className="computer device">
+                    <div className="device__inner">
+                        <div className="device__item item1"></div>
+                        <div className="device__items">
+                            <div className="device__item item2"></div>
+                            <div className="device__item item3"></div>
+                            <div className="device__item item4"></div>
+                            <div className="device__item item5"></div>    
+                        </div>
+                    </div>
+                </div>
+                <div className="phone device">
+                    <div className="device__inner">
+                        <div className="device__item phone__item1"></div>
+                        <div className="device__items phone__items">
+                            <div className="device__item phone__item2"></div>
+                            <div className="device__item phone__item3"></div>
+                            <div className="device__item phone__item4"></div>
+                            <div className="device__item phone__item5"></div>
+                            <div className="device__item phone__item6"></div>
+                            <div className="device__item phone__item7"></div>
+                        </div>
+                        <div className="phone__footer"></div>
+                    </div>
+                </div>
+            </main>
             <footer></footer>
 
         </div>
